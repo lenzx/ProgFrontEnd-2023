dac7edaf1069ea4493f34d5391c35eb218f0cc2d
Tecnologías de la Información <tic.calama@inacap.cl>
2023-07-07 14:37:31 -0400

    primer commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React,{useEffect,useState} from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <h1>Nueva Pagina Inicio</h1>
+      <p>hola desde aqui</p>
+      <Routes>
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
