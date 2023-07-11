174c72f186e0b72f634b2c1ff7d17c993238292e
Tecnologías de la Información <tic.calama@inacap.cl>
2023-07-11 11:08:30 -0400

    fix

--- src/components/Ejercicio.jsx
+++ src/components/Ejercicio.jsx
@@ -3,17 +3,16 @@ import React, {useEffect,useState} from "react"
 const Ejercicio = () =>{
 
     const [valor,setValor] = useState(0)
-    const handleValor = (evento) => {
-        setValor(document.querySelector("#numero"))
-    }
     const [respuesta,setRespuesta] = useState(0)
     const handleNumero = (evento) => {
         let numero = document.querySelector("#numero").value
-        let total = 1; 
+        let total = 1;
         for (let i=1; i<=numero; i++) {
             total = total * i; 
         }
         setRespuesta(total)
+        setValor(numero)
+        
     }
     
     return (
@@ -25,7 +24,7 @@ const Ejercicio = () =>{
             <button onClick={handleNumero}>Calcular factorial</button>
         </div>
         {
-            respuesta != 1 && <p>El factorial ${valor} es  ${respuesta}</p>
+            valor > 0  && <p>El factorial {valor} es {respuesta}</p>
         }
         <div id="respuesta">
 
